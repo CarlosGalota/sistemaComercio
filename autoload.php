af8ce9b2bc5368a6788cc4c4bc6d715657338f54
Carlos <forwa1945@gmail.com>
2024-01-23 00:06:14 -0300

    clase terminada

--- autoload.php
+++ autoload.php
@@ -1,10 +1,4 @@
 <?php
-    function controllers_autoload($classname) {
-        include 'controllers/'. $classname . '.php';
-    }
-
-    spl_autoload_register('controllers_autoload');
-?>
 
 function controllers_autoload($classname){
 	include 'controllers/' . $classname . '.php';
